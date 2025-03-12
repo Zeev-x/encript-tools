92b15b067d40a5d4effdbb6f38bd87cc8f6148e3
Zeevalya <87901027+Zeev-x@users.noreply.github.com>
2025-03-12 20:31:31 +0700

    Add files via upload

---
+++ tinstall.sh
@@ -0,0 +1,7 @@
+apt-get install openssl
+mv encript ~/../usr/bin
+sudo chmod +x ~/../usr/bin/encript
+mv decript ~/../usr/bin
+sudo chmod +x ~/../usr/bin/decript
+echo "Installasi Selesai"
+echo "Script By: Reyette"
