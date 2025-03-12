012059dd0c13c697061d4c3c38bf5b2f98b7f658
Zeevalya <87901027+Zeev-x@users.noreply.github.com>
2025-03-12 20:41:49 +0700

    Update tinstall.sh

--- tinstall.sh
+++ tinstall.sh
@@ -1,7 +1,7 @@
 apt-get install openssl
 mv encript /data/data/com.termux/files/usr/bin
-sudo chmod +x /data/data/com.termux/files/usr/bin/encript
+chmod +x /data/data/com.termux/files/usr/bin/encript
 mv decript /data/data/com.termux/files/usr/bin
-sudo chmod /data/data/com.termux/files/usr/bin/decript
+chmod /data/data/com.termux/files/usr/bin/decript
 echo "Installasi Selesai"
 echo "Script By: Reyette"
